bb892fde52338a239369e7e3c70509a3c2b4ab81
geehalel <geehalel@gmail.com>
2018-01-31 18:28:52 +0100

    velleman-pcsu1000: scan devices

--- src/hardware/velleman-pcsu1000/api.c
+++ src/hardware/velleman-pcsu1000/api.c
@@ -18,23 +18,137 @@
  */
 
 #include <config.h>
+#include <glib.h>
+#include <libusb.h>
 #include "protocol.h"
+
+static const char *channel_names[] = {
+	"CH1", "CH2",
+};
+
+static struct sr_dev_inst *pcsu1000_dev_new()
+{
+	struct sr_dev_inst *sdi;
+	struct sr_channel *ch;
+	struct sr_channel_group *cg;
+	struct dev_context *devc;
+	unsigned int i;
+
+	sdi = g_malloc0(sizeof(struct sr_dev_inst));
+	sdi->status = SR_ST_INITIALIZING;
+	sdi->vendor = g_strdup("Velleman");
+	sdi->model = g_strdup("PCSU1000");
+
+	/*
+	 * Add only the real channels -- EXT isn't a source of data, only
+	 * a trigger source internal to the device.
+	 */
+	for (i = 0; i < ARRAY_SIZE(channel_names); i++) {
+		ch = sr_channel_new(sdi, i, SR_CHANNEL_ANALOG, TRUE, channel_names[i]);
+		cg = g_malloc0(sizeof(struct sr_channel_group));
+		cg->name = g_strdup(channel_names[i]);
+		cg->channels = g_slist_append(cg->channels, ch);
+		sdi->channel_groups = g_slist_append(sdi->channel_groups, cg);
+	}
+
+	devc = g_malloc0(sizeof(struct dev_context));
+	devc->dev_state = IDLE;
+	devc->timebase = DEFAULT_TIMEBASE;
+	devc->samplerate = DEFAULT_SAMPLERATE;
+	devc->ch_enabled[0] = TRUE;
+	devc->ch_enabled[1] = TRUE;
+	devc->voltage[0] = DEFAULT_VOLTAGE;
+	devc->voltage[1] = DEFAULT_VOLTAGE;
+	devc->coupling[0] = DEFAULT_COUPLING;
+	devc->coupling[1] = DEFAULT_COUPLING;
+	devc->voffset_ch1 = DEFAULT_VERT_OFFSET;
+	devc->voffset_ch2 = DEFAULT_VERT_OFFSET;
+	devc->voffset_trigger = DEFAULT_VERT_TRIGGERPOS;
+	devc->framesize = DEFAULT_FRAMESIZE;
+	devc->triggerslope = SLOPE_POSITIVE;
+	devc->triggersource = g_strdup(DEFAULT_TRIGGER_SOURCE);
+	devc->capture_ratio = DEFAULT_CAPTURE_RATIO;
+	sdi->priv = devc;
+
+	return sdi;
+}
+
 
 static GSList *scan(struct sr_dev_driver *di, GSList *options)
 {
 	struct drv_context *drvc;
 	GSList *devices;
-
-	(void)options;
+	struct dev_context *devc;
+	struct sr_dev_inst *sdi;
+	struct sr_usb_dev_inst *usb;
+	struct sr_config *src;
+	GSList *l;
+	GSList *conn_devices;
+	struct libusb_device_descriptor des;
+	libusb_device **devlist;
+	int i;
+	const char *conn;
+	char connection_id[64];
 
 	devices = NULL;
 	drvc = di->context;
 	drvc->instances = NULL;
+	conn = NULL;
+	for (l = options; l; l = l->next) {
+		src = l->data;
+		if (src->key == SR_CONF_CONN) {
+			conn = g_variant_get_string(src->data, NULL);
+			break;
+		}
+	}
+	if (conn)
+		conn_devices = sr_usb_find(drvc->sr_ctx->libusb_ctx, conn);
+	else
+		conn_devices = NULL;
 
-	/* TODO: scan for devices, either based on a SR_CONF_CONN option
-	 * or on a USB scan. */
+	/* Find all Velleman PCSU1000 devices and upload firmware to all of them. */
+	libusb_get_device_list(drvc->sr_ctx->libusb_ctx, &devlist);
+	for (i = 0; devlist[i]; i++) {
+		if (conn) {
+			usb = NULL;
+			for (l = conn_devices; l; l = l->next) {
+				usb = l->data;
+				if (usb->bus == libusb_get_bus_number(devlist[i])
+					&& usb->address == libusb_get_device_address(devlist[i]))
+					break;
+			}
+			if (!l)
+				/* This device matched none of the ones that
+				 * matched the conn specification. */
+				continue;
+		}
 
-	return devices;
+		libusb_get_device_descriptor(devlist[i], &des);
+
+		usb_get_port_path(devlist[i], connection_id, sizeof(connection_id));
+
+		if (des.idVendor == VELLEMAN_PCSU1000_VID
+			&& des.idProduct == VELLEMAN_PCSU1000_PID) {
+			/* Device matches the Vid/Pid. */
+			sr_dbg("Found a %s %s.", "Velleman", "PCSU1000");
+			sdi = pcsu1000_dev_new();
+			sdi->connection_id = g_strdup(connection_id);
+			devices = g_slist_append(devices, sdi);
+			devc = sdi->priv;
+			//if (ezusb_upload_firmware(drvc->sr_ctx, devlist[i],
+			//				USB_CONFIGURATION, prof->firmware) == SR_OK)
+				/* Remember when the firmware on this device was updated */
+			//	devc->fw_updated = g_get_monotonic_time();
+			//else
+			//	sr_err("Firmware upload failed");
+			/* Dummy USB address of 0xff will get overwritten later. */
+			sdi->conn = sr_usb_dev_inst_new(
+				libusb_get_bus_number(devlist[i]), 0xff, NULL);
+		}
+	}
+	libusb_free_device_list(devlist, 1);
+
+	return std_scan_complete(di, devices);
 }
 
 static int dev_open(struct sr_dev_inst *sdi)
--- src/hardware/velleman-pcsu1000/protocol.h
+++ src/hardware/velleman-pcsu1000/protocol.h
@@ -27,7 +27,131 @@
 
 #define LOG_PREFIX "velleman-pcsu1000"
 
+#define VELLEMAN_PCSU1000_VID 0x10cf
+#define VELLEMAN_PCSU1000_PID 0x1000
+#define VELLEMAN_PCSU1000_FIRMWARE "pcsu1000.bit"
+
+#define NUM_CHANNELS            2
+
+/* Must match the coupling table. */
+enum couplings {
+	COUPLING_AC = 0,
+	COUPLING_DC,
+	COUPLING_GND,
+};
+
+/* Must match the timebases table. */
+enum time_bases {
+	TIME_100ns = 0,
+	TIME_200ns,
+	TIME_500ns,
+	TIME_1us,
+	TIME_2us,
+	TIME_5us,
+	TIME_10us,
+	TIME_20us,
+	TIME_50us,
+	TIME_1ms,
+	TIME_2ms,
+	TIME_5ms,
+	TIME_10ms,
+	TIME_20ms,
+	TIME_50ms,
+	TIME_100ms,
+	TIME_200ms,
+	TIME_500ms,
+};
+
+/* Must match the vdivs table. */
+enum {
+	VDIV_5MV,
+	VDIV_10MV,
+	VDIV_20MV,
+	VDIV_50MV,
+	VDIV_100MV,
+	VDIV_200MV,
+	VDIV_500MV,
+	VDIV_1V,
+	VDIV_2V,
+};
+
+enum trigger_slopes {
+	SLOPE_POSITIVE = 0,
+	SLOPE_NEGATIVE,
+};
+
+enum trigger_sources {
+	TRIGGER_CH1 = 0,
+	TRIGGER_CH2,
+	TRIGGER_EXT,
+};
+
+enum capturestates {
+	CAPTURE_EMPTY = 0,
+	CAPTURE_FILLING = 1,
+	CAPTURE_READY_8BIT = 2,
+	CAPTURE_TIMEOUT = 127,
+	CAPTURE_UNKNOWN = 255,
+};
+
+enum triggermodes {
+	TRIGGERMODE_AUTO,
+	TRIGGERMODE_NORMAL,
+	TRIGGERMODE_SINGLE,
+};
+
+enum states {
+	IDLE,
+	NEW_CAPTURE,
+	CAPTURE,
+	FETCH_DATA,
+	STOPPING,
+};
+
+#define DEFAULT_VOLTAGE         VDIV_1V
+#define DEFAULT_FRAMESIZE       FRAMESIZE_SMALL
+#define DEFAULT_TIMEBASE        TIME_1ms
+#define DEFAULT_SAMPLERATE      SR_KHZ(1)
+#define DEFAULT_TRIGGER_SOURCE  "CH1"
+#define DEFAULT_COUPLING        COUPLING_DC
+#define DEFAULT_CAPTURE_RATIO   100
+#define DEFAULT_VERT_OFFSET     0.5
+#define DEFAULT_VERT_TRIGGERPOS 0.5
+#define FRAMESIZE_SMALL         (4 * 1024)
+
+
 struct dev_context {
+	uint64_t limit_frames;
+	uint64_t num_frames;
+	GSList *enabled_channels;
+	int64_t fw_updated;
+	int epin_maxpacketsize;
+	int capture_empty_count;
+	int dev_state;
+
+	/* Oscilloscope settings. */
+	uint64_t samplerate;
+	int timebase;
+	gboolean ch_enabled[2];
+	int voltage[2];
+	int coupling[2];
+	// voltage offset (vertical position)
+	float voffset_ch1;
+	float voffset_ch2;
+	float voffset_trigger;
+	uint16_t channel_levels[2][9][2];
+	unsigned int framesize;
+	gboolean filter[2];
+	int triggerslope;
+	char *triggersource;
+	uint64_t capture_ratio;
+	int triggermode;
+
+	/* Frame transfer */
+	unsigned int samp_received;
+	unsigned int samp_buffered;
+	unsigned int trigger_offset;
+	unsigned char *framebuf;
 };
 
 SR_PRIV int velleman_pcsu1000_receive_data(int fd, int revents, void *cb_data);
